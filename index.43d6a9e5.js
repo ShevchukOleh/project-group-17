426af2471a68901ff15fb417581ceb03daf443b8
ShevchukOleh <115993748+ShevchukOleh@users.noreply.github.com>
2022-12-27 08:24:37 +0000

    Deploying to gh-pages from @ ShevchukOleh/project-group-17@ff5bd448a5fe58b60ac34d45fcdb9f1e07f52c2e 🚀

--- index.43d6a9e5.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-about-product-modal-open]"),closeModalBtn:document.querySelector("[data-about-product-modal-close]"),modal:document.querySelector("[data-about-product-modal]")};function t(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",t),o.closeModalBtn.addEventListener("click",t)}();
-//# sourceMappingURL=index.43d6a9e5.js.map
